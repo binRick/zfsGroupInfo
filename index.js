6b9cb9d1a2e1c3ccef94abc17bdf026c57c50f36
Rick <rick@Ricks-MacBook-Air.local>
2015-07-22 18:41:50 -0600

    iii

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var CachemanFile = require('cacheman-file'),
+    md5=require('md5'),
     chalk = require('chalk'),
     _ = require('underscore'),
     child = require('child_process');
@@ -8,23 +9,30 @@ var debug = true;
 module.exports = function(Setup, CB) {
     Setup = Setup || {};
     CB = CB || function() {};
+	var pre = 'ssh beo';
     Setup.uniqe = Setup.uniq || false;
     Setup.cache = Setup.cache || new CachemanFile();
     Setup.field = Setup.field || false;
     Setup.delimiter = Setup.delimiter || '/';
     Setup.ttl = Setup.ttl || 300;
-    Setup.key = Setup.key || 'myZfsGroup';
     Setup.server = Setup.server || 'intrepid';
     Setup.matches = Setup.matches || ['^bank/Voyager/[0-9]*.[0-9]$'];
-
-    var cmd = 'ssh ' + Setup.server + ' zfs list -H -o name | egrep "' + Setup.matches.join('|') + '"';
+    Setup.bin = Setup.bin || 'zfs';
+	Setup.args = Setup.args || 'list -H -o name';
+    var cmd = pre +' ' + 'ssh ' + Setup.server + ' ' + Setup.bin +  ' ' + Setup.args + ' | egrep "' + Setup.matches.join('|') + '"';
+    Setup.key = md5(Setup.cmd + Setup.server+ Setup.args);
+    //JSON.stringify(Setup));
     if (debug) console.log(chalk.green.bgBlack(cmd));
     Setup.cache.get(Setup.key, function(e, Filesystems) {
         if (e) throw e;
         Filesystems = _.toArray(Filesystems);
         if (!Filesystems || Filesystems.length < 1) {
             child.execSync = child.execSync || require('exec-sync');
-            Filesystems = child.execSync(cmd).toString().split('\n').filter(function(l) {
+            Filesystems = child.execSync(cmd).toString();
+	if(Filesystems.split('\n').length==0)
+		Filesystems=[Filesystems];
+	else
+	    Filesystems = Filesystems.split('\n').filter(function(l) {
                 return l.length > 0;
             });
             Setup.cache.set(Setup.key, Filesystems, Setup.ttl, function(e) {
